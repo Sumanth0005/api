e1afdbee4e3001f2db923c779c438dece7a0eac6
Sumanth0005 <sumanthkurapati13@gmail.com>
2025-01-19 22:45:21 +0530

    crud

--- src/routes/badgeRoutes.js
+++ src/routes/badgeRoutes.js
@@ -3,5 +3,8 @@ const badgeController = require('../controllers/admin/badgeController');
 const router = express.Router();
 
 router.post('/', badgeController.add);
+router.put('/update/:id', badgeController.updateBadge);
+router.delete('/badge/:id', badgeController.deleteBadge);
+router.get('/badge/:id', badgeController.readBadge);
 
 module.exports = router;
--- src/services/badgeService.js
+++ src/services/badgeService.js
@@ -1,6 +1,22 @@
+const badge = require('../models/admin/badge');
 const Badge = require('../models/admin/badge');
 
 exports.addBadge = async (badgeData) => {
   const badge = new Badge(badgeData);
   return await badge.save();
+};
+exports.update = async (badgeData, badgeId) => {
+  return await badge.findByIdAndUpdate(
+    badgeId,
+    { $set: badgeData}, 
+    { new: true, runValidators: true } 
+  );  
+}
+
+exports.get = async (id) => {
+  return await badge.findById(id);
+};
+
+exports.delete = async (id) => {
+  return await badge.findByIdAndDelete(id);
 };
