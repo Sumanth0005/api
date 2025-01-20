8a7b3a8077a47b9c3d059a00be868209548cd8f5
Sumanth0005 <sumanthkurapati13@gmail.com>
2025-01-20 11:58:43 +0530

    retrived all batches

--- src/routes/badgeRoutes.js
+++ src/routes/badgeRoutes.js
@@ -6,5 +6,6 @@ router.post('/', badgeController.add);
 router.put('/update/:id', badgeController.updateBadge);
 router.delete('/badge/:id', badgeController.deleteBadge);
 router.get('/badge/:id', badgeController.readBadge);
+router.get('/get', badgeController.readAllBadges);
 
 module.exports = router;
