d447ae602866f2cdb75c26c7f92b76f0de753c7d
Sumanth0005 <sumanthkurapati13@gmail.com>
2025-01-25 13:17:42 +0530

    crud

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authenticateToken = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.put('/update/:id', userController.updateUser);
-router.get('/:id', userController.readUser);
+router.put('/update/:id',authenticateToken, userController.updateUser);
+router.get('/:id',  authenticateToken, userController.readUser);
 
 module.exports = router;
